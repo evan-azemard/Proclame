c2f715361a8771ce7c5a65df3a638525aafd72f7
evan-azemard <evan.thomas.azemard@gmail.com>
2025-08-26 17:54:57 +0200

    fix(CardProclamation): add arialabel, fix error navigate

--- packages/client/src/components/molecules/CardProclamation/CardProclamation.tsx
+++ packages/client/src/components/molecules/CardProclamation/CardProclamation.tsx
@@ -1,35 +1,43 @@
-import type { CardProclamationProps } from './CardProclamation.props';
-import styles from './CardProclamation.module.scss';
-import { Icon, Title } from '@atoms/index';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import type { CardProclamationProps } from "./CardProclamation.props";
+import styles from "./CardProclamation.module.scss";
+import { Icon, Title } from "@atoms/index";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function CardProclamation({ title, description, uri }: CardProclamationProps) {
+export default function CardProclamation({
+  title,
+  description,
+  uri,
+}: CardProclamationProps) {
   const [isFavorite, setIsFavorite] = useState(false);
 
   const navigate = useNavigate();
 
   const handleIconClick = () => {
-    console.log("first");
     setIsFavorite((prev) => !prev);
   };
 
   const handleNavigate = () => {
-    navigate(uri)
-  }
+    navigate(uri);
+  };
 
   return (
-    <article className={styles.article} onClick={handleNavigate}>
-      <header>
+    <article
+      className={styles.article}
+      role="article"
+      aria-label={`Proclamation: ${title}`}
+    >
+      <header onClick={handleNavigate}>
         <Title type={2}>{title}</Title>
-        <Icon
-          name="favorite"
-          title="favori"
-          color={isFavorite ? '#ffb44c' : 'currentColor'}
-          onClick={handleIconClick}
-        />
       </header>
-      <main>
+      <Icon
+        name="favorite"
+        title="favori"
+        color={isFavorite ? "#ffb44c" : "currentColor"}
+        onClick={handleIconClick} // Appelle la fonction avec l'événement
+        aria-label={isFavorite ? "Retirer des favoris" : "Ajouter aux favoris"}
+      />
+      <main onClick={handleNavigate}>
         <p>{description}</p>
       </main>
     </article>
