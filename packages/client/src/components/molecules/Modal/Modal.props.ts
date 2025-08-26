cc7706c6126a3c2016c373f811ea928bc0a48f95
evan-azemard <evan.thomas.azemard@gmail.com>
2025-08-26 22:48:20 +0200

    feat(Modal): add Modal molecule

--- packages/client/src/components/molecules/Modal/Modal.props.ts
+++ packages/client/src/components/molecules/Modal/Modal.props.ts
@@ -1,1 +1,8 @@
-export interface ModalProps { children?: React.ReactNode }
+export type ModalButton = { name: string; onClick?: () => void };
+
+export type ModalProps = {
+  show: boolean;                 
+  children: string;             
+  btn1?: ModalButton;   
+  btn2?: ModalButton; 
+};
--- packages/client/src/components/molecules/Modal/Modal.tsx
+++ packages/client/src/components/molecules/Modal/Modal.tsx
@@ -1,6 +1,39 @@
-import type { ModalProps } from './Modal.props';
-import styles from './Modal.module.scss';
+import type { ModalProps } from "./Modal.props";
+import styles from "./Modal.module.scss";
+import { LongueText } from "@atoms/index";
 
-export default function Modal({ children }: ModalProps) {
-  return <div className={styles.modal}>{children}</div>;
+export default function Modal({ show, children, btn1, btn2 }: ModalProps) {
+  if (!show) return null;
+
+  const handleBtn1 = () => {
+    btn1?.onClick?.();
+  };
+  const handleBtn2 = () => {
+    btn2?.onClick?.();
+  };
+
+  return (
+    <dialog className={styles.dialog} open aria-modal="true">
+      <div>
+        <LongueText>{children}</LongueText>
+      </div>
+
+      {(btn1 || btn2) && <hr />}
+      <div className={styles.buttons}>
+        {btn1 && (
+          <button type="button" onClick={handleBtn1}>
+            {btn1.name}
+          </button>
+        )}
+        {btn2 && (
+          <div>
+            <span></span>
+            <button type="button" onClick={handleBtn2}>
+              {btn2.name}
+            </button>
+          </div>
+        )}
+      </div>
+    </dialog>
+  );
 }
