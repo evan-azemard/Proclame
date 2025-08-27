48ba8e5b641ca119407529a52a7ec62a1957ecba
evan-azemard <evan.thomas.azemard@gmail.com>
2025-08-27 21:34:19 +0200

    feat'(privacyPolicy): add PrivacyPolicy template

--- packages/client/src/components/templates/PrivacyPolicy/PrivacyPolicy.template.tsx
+++ packages/client/src/components/templates/PrivacyPolicy/PrivacyPolicy.template.tsx
@@ -1,3 +1,59 @@
+import { Button, Wave } from "@atoms/index";
+import { TitleWithParagraph } from "@molecules/index";
+
 export default function PrivacyPolicy() {
-  return <h1>PrivacyPolicy</h1>;
+  return (
+    <>
+      <Button isBack />
+      <Wave space />
+      <TitleWithParagraph title="Politique de confidentialité">
+        <p>
+          L’application collecte uniquement les données nécessaires à son bon
+          fonctionnement, notamment votre nom d’utilisateur et votre adresse
+          email lors de l’inscription.
+        </p>
+        <p>
+          Ces informations sont utilisées pour vous identifier, personnaliser
+          votre expérience et assurer la sécurité du service.
+        </p>
+        <p>
+          Aucune donnée n’est vendue ou partagée avec des tiers.
+          <br />
+        </p>
+        <p>
+          Les informations sont stockées de manière sécurisée et supprimées
+          automatiquement à la déconnexion ou sur demande.
+        </p>
+        <p>
+          Vous pouvez supprimer votre compte à tout moment depuis la page
+          “Profil”.
+        </p>
+        <p>
+          Il est également possible de demander une copie de vos données
+          personnelles par email, directement depuis cette même page.
+        </p>
+        <p>
+          Des cookies essentiels sont utilisés pour maintenir votre session
+          active. Leur suppression via l’application entraînera une déconnexion
+          immédiate.
+        </p>
+        <p>
+          Certains contenus affichés, comme les textes bibliques, sont libres de
+          droits.
+        </p>
+        <p>
+          En revanche, l’interface, les fonctionnalités et les données liées à
+          votre compte ne peuvent être réutilisées sans autorisation.
+        </p>
+        <p>Cette politique peut être modifiée à tout moment, sans préavis.</p>
+        <p>
+          En continuant à utiliser l’application, vous acceptez les éventuelles
+          mises à jour.
+        </p>
+      </TitleWithParagraph>
+      <div className="flex-end">
+        <Button text="Je ne suis pas d’accord" />
+      </div>
+    </>
+  );
 }
