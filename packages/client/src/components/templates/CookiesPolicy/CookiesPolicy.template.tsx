a4bd0acd89d408c40f4162c08408ee5dfc962efd
evan-azemard <evan.thomas.azemard@gmail.com>
2025-08-27 22:16:04 +0200

    feat(cookie)-fix(login): add Cookis template, fix style to login

--- packages/client/src/components/templates/CookiesPolicy/CookiesPolicy.template.tsx
+++ packages/client/src/components/templates/CookiesPolicy/CookiesPolicy.template.tsx
@@ -1,3 +1,44 @@
+import { Button, Wave } from "@atoms/index";
+import { TitleWithParagraph } from "@molecules/index";
+
 export default function CookiesPolicy() {
-  return <h1>CookiesPolicy</h1>;
+  return (
+    <>
+      <Button isBack />
+      <Wave space />
+      <TitleWithParagraph title="Politique des cookis">
+        <p>
+          Cette application utilise des cookies essentiels au bon fonctionnement
+          de ses services.
+        </p>
+
+        <p>
+          <strong>1. Cookies d’authentification</strong>
+          <br />
+          Des cookies sont utilisés pour enregistrer votre session et vous
+          permettre de rester connecté(e) pendant votre utilisation de
+          l’application.
+        </p>
+
+        <p>
+          <strong>2. Cookies de session utilisateur</strong>
+          <br />
+          Pendant votre session, le nom d’utilisateur et l’adresse email sont
+          temporairement stockés afin d’améliorer votre expérience. Ces données
+          sont automatiquement supprimées à la déconnexion.
+        </p>
+
+        <p>
+          <strong>3. Suppression des cookies</strong>
+          <br />
+          La suppression des cookies via l’application entraînera une
+          déconnexion immédiate, car ces cookies sont indispensables au
+          fonctionnement des services.
+        </p>
+      </TitleWithParagraph>
+      <div className="flex-end">
+        <Button text="Je ne suis pas d’accord" />
+      </div>
+    </>
+  );
 }
--- packages/client/src/components/templates/Login/Login.template.tsx
+++ packages/client/src/components/templates/Login/Login.template.tsx
@@ -10,16 +10,34 @@ export default function Login() {
       <SectionForm type="login" />
       <Wave />
       <TitleWithParagraph title="Politique des cookis">
-        Cette application utilise des cookies essentiels au bon fonctionnement
-        de ses services. 1. Cookies d’authentification Des cookies sont utilisés
-        pour enregistrer votre session et vous permettre de rester connecté(e)
-        pendant votre utilisation de l’application. 2. Cookies de session
-        utilisateur Pendant votre session, le nom d’utilisateur et l’adresse
-        email sont temporairement stockés afin d’améliorer votre expérience. Ces
-        données sont automatiquement supprimées à la déconnexion. 3. Suppression
-        des cookies La suppression des cookies via l’application entraînera une
-        déconnexion immédiate, car ces cookies sont indispensables au
-        fonctionnement des services.
+        <p>
+          Cette application utilise des cookies essentiels au bon fonctionnement
+          de ses services.
+        </p>
+
+        <p>
+          <strong>1. Cookies d’authentification</strong>
+          <br />
+          Des cookies sont utilisés pour enregistrer votre session et vous
+          permettre de rester connecté(e) pendant votre utilisation de
+          l’application.
+        </p>
+
+        <p>
+          <strong>2. Cookies de session utilisateur</strong>
+          <br />
+          Pendant votre session, le nom d’utilisateur et l’adresse email sont
+          temporairement stockés afin d’améliorer votre expérience. Ces données
+          sont automatiquement supprimées à la déconnexion.
+        </p>
+
+        <p>
+          <strong>3. Suppression des cookies</strong>
+          <br />
+          La suppression des cookies via l’application entraînera une
+          déconnexion immédiate, car ces cookies sont indispensables au
+          fonctionnement des services.
+        </p>
       </TitleWithParagraph>
       <div className="flex-end">
         <Button text="Accepter" />
