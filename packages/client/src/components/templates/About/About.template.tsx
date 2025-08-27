d2a874bdd68b0b4e5b31df6b628a904b835c7e7c
evan-azemard <evan.thomas.azemard@gmail.com>
2025-08-27 22:10:50 +0200

    feat(about): add About template

--- packages/client/src/components/templates/About/About.template.tsx
+++ packages/client/src/components/templates/About/About.template.tsx
@@ -1,3 +1,51 @@
+import { Button, Title } from "@atoms/index";
+import { TitleWithParagraph } from "@molecules/index";
+import styles from "./About.module.scss";
+import logo from "@atoms/Icon/icons/logo.png";
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <Button isBack />
+      <div className={styles.logo}>
+        <img src={logo} alt="Proclame logo" className={styles.logoImg} />
+        <div className={styles.logoTitle}>
+          <Title>PROCLAME</Title>
+        </div>
+      </div>
+      <TitleWithParagraph title="A propos">
+        <p>
+          Hey! moi c’est Evan. J’ai 24 ans et je suis en formation en
+          développement web.
+        </p>
+        <p>
+          Proclame est créé avec passion, foi et engagement. Il s’agit d’une
+          application pensée pour offrir un soutien émotionnel à travers des
+          textes bibliques, dans un format immersif et apaisant.
+        </p>
+        <p>
+          Chaque proclamation est accompagnée d’une ambiance sonore
+          personnalisable, pour favoriser la méditation, le réconfort et la paix
+          intérieure.
+        </p>
+        <p>
+          J’ai réalisé l’ensemble du développement, du design à la mise en
+          ligne, en utilisant des technologies modernes pour garantir
+          performance, fiabilité et sécurité.
+        </p>
+        <p>
+          L’application respecte les derniers standards en matière de protection
+          des données et de sécurité web, avec une attention particulière portée
+          à la robustesse du code et à la confidentialité des utilisateurs.
+        </p>
+        <p>
+          Dans une démarche responsable, j’ai aussi réduit l’empreinte carbone
+          de l’application grâce à des principes d’éco-conception.
+        </p>
+        <p>
+          Chaque détail a été travaillé avec soin, dans l’idée de proposer un
+          outil sincère, utile et durable.
+        </p>
+      </TitleWithParagraph>
+    </>
+  );
 }
