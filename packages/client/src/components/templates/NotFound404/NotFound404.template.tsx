0c3b31d935e2303f47c90cf6adaa4c911534f59c
evan-azemard <evan.thomas.azemard@gmail.com>
2025-08-28 10:00:12 +0200

    feat(404): add 404 template

--- packages/client/src/components/templates/NotFound404/NotFound404.template.tsx
+++ packages/client/src/components/templates/NotFound404/NotFound404.template.tsx
@@ -1,3 +1,28 @@
+import { Button, Wave } from "@atoms/index";
+import { TitleWithParagraph } from "@molecules/index";
+
 export default function NotFound404() {
-  return <h1>NotFound404</h1>;
+  return (
+    <>
+      <Button isBack />
+      <Wave space />
+      <TitleWithParagraph
+        title="Cette page semble 
+introuvable"
+      >
+        <p>
+          Tu n’es pas censé être ici, mais tout va bien. Parfois, on cherche
+          quelque chose… et on tombe ailleurs.
+        </p>
+        <p>
+          Ce n’est peut-être pas l’endroit prévu, mais qui sait ce que tu
+          pourrais y découvrir ?
+        </p>
+      </TitleWithParagraph>
+
+      <div className="flex-end">
+        <Button text="Accueil" to="/" />
+      </div>
+    </>
+  );
 }
