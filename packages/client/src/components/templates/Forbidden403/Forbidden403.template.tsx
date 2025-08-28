64f60818921b167a65c2d430f55adf89c5bafce3
evan-azemard <evan.thomas.azemard@gmail.com>
2025-08-28 10:03:01 +0200

    feat(403): add 403 template

--- packages/client/src/components/templates/Forbidden403/Forbidden403.template.tsx
+++ packages/client/src/components/templates/Forbidden403/Forbidden403.template.tsx
@@ -1,3 +1,24 @@
+import { Button, Wave } from "@atoms/index";
+import { TitleWithParagraph } from "@molecules/index";
+
 export default function Forbidden403() {
-  return <h1>Forbidden403</h1>;
+  return (
+    <>
+      <Button isBack />
+      <Wave space />
+      <TitleWithParagraph title="Tu n’es pas censé être ici">
+        <p>
+          Mais pas de panique, tout va bien. Parfois, les portes se ferment…
+          mais ça ne veut pas dire qu’il n’y a rien derrière.
+        </p>
+        <p>
+          Peut-être que ce chemin n’était pas le bon, ou simplement pas le bon
+          moment.
+        </p>
+      </TitleWithParagraph>
+      <div className="flex-end">
+        <Button text="Accueil" to="/" />
+      </div>
+    </>
+  );
 }
