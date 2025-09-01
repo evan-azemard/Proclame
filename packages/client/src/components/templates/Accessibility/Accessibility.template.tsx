3e6258f4d54f11a129c75a799bc03856e116c0c9
evan-azemard <evan.thomas.azemard@gmail.com>
2025-09-01 13:41:13 +0200

    feat(accessibility): add Accessibility Temlate and add switch input

--- packages/client/src/components/templates/Accessibility/Accessibility.template.tsx
+++ packages/client/src/components/templates/Accessibility/Accessibility.template.tsx
@@ -1,10 +1,63 @@
-import { Container, Title } from '@atoms/index';
+import { Container, MultiInput, Wave, Button, Title } from "@atoms/index";
 
 export default function Accessibility() {
   return (
     <Container>
-      <Title type={1} id="accessibility-title">Accessibilité</Title>
-      <p aria-describedby="accessibility-title">Bienvenue sur la page d'accessibilité. Ici, vous trouverez des informations sur les fonctionnalités d'accessibilité de notre site.</p>
+      <Wave />
+      <form>
+        <MultiInput
+          type="select"
+          name="fontFamily"
+          label="Choix de la police"
+          placeholder="Défaut"
+          options={["Lexend", "OpenDyslexic", "OpenSans", "Verdana"]}
+        />
+        <MultiInput
+          type="select"
+          name="fontSize"
+          label="Taille de la police"
+          placeholder="Défaut"
+          options={["17", "18", "19", "20"]}
+        />
+        <MultiInput
+          type="select"
+          name="fontColor"
+          label="Couleur de la police"
+          placeholder="Défaut"
+          options={["Noir", "Blanc", "Bleu", "Rouge", "Vert"]}
+        />
+        <MultiInput
+          type="select"
+          name="backgroundColor"
+          label="Couleur de fond"
+          placeholder="Défaut"
+          options={["Blanc", "Noir", "Gris clair", "Gris foncé"]}
+        />
+        <MultiInput
+          type="select"
+          name="mainColor"
+          label="Couleur principale"
+          placeholder="Défaut"
+          options={["Bleu", "Rouge", "Vert", "Orange", "Violet"]}
+        />
+        <Wave />
+        <Title type={2}>Modes d’accessibilité</Title>
+        <br />
+
+        <MultiInput type="switch" name="seniorMode" label="Mode senior" />
+        <MultiInput type="switch" name="daltonianMode" label="Mode daltonien" />
+        <MultiInput type="switch" name="contrastMode" label="Mode contrasté" />
+        <Wave />
+        <Title type={3}>Thème</Title>
+        <br />
+        <MultiInput
+          type="switch"
+          name="darkTheme"
+          label="Thème sombre / clair"
+        />
+        <Wave />
+        <Button text="Réinitialiser" />
+      </form>
     </Container>
   );
 }
