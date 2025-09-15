7cc27942528f77c291d235b4ae223f4c69b3d369
evan-azemard <evan.thomas.azemard@gmail.com>
2025-09-15 17:28:35 +0200

    feat(role): add type role.controller

--- packages/server/src/types/roles/role.controller.interface.ts
+++ packages/server/src/types/roles/role.controller.interface.ts
@@ -1,2 +1,18 @@
-export interface RoleController {}
+import { Role, UpdateRole } from "@/entities";
+import { Request, Response } from "express";
 
+export interface RoleController {
+  getAll: (
+    req: Request,
+    res: Response<Role[] | { message: string }>
+  ) => Promise<void>;
+  create: (
+    req: Request<{}, {}, Role>,
+    res: Response<Role | { message: string }>
+  ) => Promise<void>;
+  update: (
+    req: Request<{}, { roleId: string }, UpdateRole>,
+    res: Response<Role | { message: string }>
+  ) => Promise<void>;
+  remove: (req: Request, res: Response<{ message: string }>) => Promise<void>;
+}
