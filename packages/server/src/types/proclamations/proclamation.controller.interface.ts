cd6fbda6c03d61a89da5ef5137e92a10892ed384
evan-azemard <evan.thomas.azemard@gmail.com>
2025-09-15 17:40:10 +0200

    feat(proclamation): add proclamation.controller interface

--- packages/server/src/types/proclamations/proclamation.controller.interface.ts
+++ packages/server/src/types/proclamations/proclamation.controller.interface.ts
@@ -1,2 +1,24 @@
-export interface ProclamationController {}
-
+import { NewProclamation, Proclamation, UpdateProclamation } from "@/entities";
+import { Request, Response } from "express";
+export interface ProclamationController {
+  getAll: (
+    req: Request<{}>,
+    res: Response<Proclamation[] | { message: string }>
+  ) => Promise<void>;
+  getById: (
+    req: Request<{ id: string }>,
+    res: Response<Proclamation | { message: string }>
+  ) => Promise<void>;
+  create: (
+    req: Request<{}, {}, NewProclamation>,
+    res: Response<Proclamation | { message: string }>
+  ) => Promise<void>;
+  update: (
+    req: Request<{ id: string }, {}, UpdateProclamation>,
+    res: Response<Proclamation | { message: string }>
+  ) => Promise<void>;
+  remove: (
+    req: Request<{ id: string }, {}, {}>,
+    res: Response<{ message: string }>
+  ) => Promise<void>;
+}
