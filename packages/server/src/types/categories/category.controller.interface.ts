2bf9900f84932d5c030653b87b2fc7c5b8074a98
evan-azemard <evan.thomas.azemard@gmail.com>
2025-09-15 17:56:47 +0200

    feat(category): add category interface

--- packages/server/src/types/categories/category.controller.interface.ts
+++ packages/server/src/types/categories/category.controller.interface.ts
@@ -1,2 +1,24 @@
-export interface CategoryController {}
-
+import { Category, NewCategory, UpdateCategory } from "@/entities";
+import { Request, Response } from "express";
+export interface CategoryController {
+  getAll: (
+    req: Request<{}>,
+    res: Response<Category[] | { message: string }>
+  ) => Promise<void>;
+  getById: (
+    req: Request<{ id: string }>,
+    res: Response<Category | { message: string }>
+  ) => Promise<void>;
+  create: (
+    req: Request<{}, {}, NewCategory>,
+    res: Response<Category | { message: string }>
+  ) => Promise<void>;
+  update: (
+    req: Request<{ id: string }, {}, UpdateCategory>,
+    res: Response<Category | { message: string }>
+  ) => Promise<void>;
+  remove: (
+    req: Request<{ id: string }, {}, {}>,
+    res: Response<{ message: string }>
+  ) => Promise<void>;
+}
