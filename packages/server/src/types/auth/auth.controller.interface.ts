d4f337d3d54d5a93d7c701b4463ce225fa29c7bf
evan-azemard <evan.thomas.azemard@gmail.com>
2025-09-24 23:25:55 +0200

    feat(auth): add auth controller and auth midlleware

--- packages/server/src/types/auth/auth.controller.interface.ts
+++ packages/server/src/types/auth/auth.controller.interface.ts
@@ -2,7 +2,7 @@ import { NewUser } from "@/entities";
 import { Request, Response } from "express";
 
 export interface AuthController {
-  me: (req: Request, res: Response) => Promise<void>;
+  me: (req: Request & {user: {id: string, roleId: string}}, res: Response) => Promise<void>;
   register: (req: Request<{}, {}, NewUser>, res: Response) => Promise<void>;
   login: (
     req: Request<{}, {}, { email: string; password: string }>,
