c0055ed442cc8cdf4366439ff1986bdc8df3ee0c
evan-azemard <evan.thomas.azemard@gmail.com>
2025-09-29 12:03:01 +0200

    feat(validation): add roleCreateValidation and roleUpdateValidation

--- packages/server/src/validations/role.validation.ts
+++ packages/server/src/validations/role.validation.ts
@@ -1,3 +1,11 @@
 import { z } from "zod";
 
-// TODO: Add validation schemas for role controller methods
+export const roleCreateValidation = z.object({
+  label: z.string().min(2, { message: "Le libellé doit contenir au moins 2 caractères." }).max(50, { message: "Le libellé ne doit pas dépasser 50 caractères." }),
+  description: z.string().min(10, { message: "La description doit contenir au moins 10 caractères." }).max(255, { message: "La description ne doit pas dépasser 255 caractères." }).optional(),
+});
+
+export const roleUpdateValidation = z.object({
+  label: z.string().min(2, { message: "Le libellé doit contenir au moins 2 caractères." }).max(50, { message: "Le libellé ne doit pas dépasser 50 caractères." }).optional(),
+  description: z.string().min(10, { message: "La description doit contenir au moins 10 caractères." }).max(255, { message: "La description ne doit pas dépasser 255 caractères." }).optional(),
+});
