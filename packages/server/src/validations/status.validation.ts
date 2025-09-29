717626c43fb2e2be4641df92603c4d593eb2abe8
evan-azemard <evan.thomas.azemard@gmail.com>
2025-09-29 11:47:19 +0200

    feat(status): add statusCreateValidation and statusUpdateValidation

--- packages/server/src/validations/status.validation.ts
+++ packages/server/src/validations/status.validation.ts
@@ -1,3 +1,30 @@
 import { z } from "zod";
 
-// TODO: Add validation schemas for status controller methods
+export const statusCreateValidation = z.object({
+  label: z
+    .string()
+    .min(2, { message: "Le libéllé doit contenir au moins 2 caractères" })
+    .max(50, { message: "Le libellé doit contenir au maximum 50 caractères" }),
+  description: z
+    .string()
+    .min(5, { message: "La description doit contenir au moins 5 caractères" })
+    .max(255, {
+      message: "La description doit contenir au maximum 255 caractères",
+    })
+    .optional(),
+});
+
+
+export const statusUpdateValidation = z.object({
+  label: z
+    .string()
+    .min(2, { message: "Le libéllé doit contenir au moins 2 caractères" })
+    .max(50, { message: "Le libellé doit contenir au maximum 50 caractères" }),
+  description: z
+    .string()
+    .min(5, { message: "La description doit contenir au moins 5 caractères" })
+    .max(255, {
+      message: "La description doit contenir au maximum 255 caractères",
+    })
+    .optional(),
+});
