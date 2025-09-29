c4c8a3ddddf336569add989ff7c25377304d1d98
evan-azemard <evan.thomas.azemard@gmail.com>
2025-09-29 11:15:57 +0200

    feat(userUpdateValidation): add max size

--- packages/server/src/validations/user.validation.ts
+++ packages/server/src/validations/user.validation.ts
@@ -2,11 +2,19 @@ import { z } from "zod";
 
 export const userUpdateSchema = z
   .object({
-    email: z.string().email({ message: "L'email n'est pas valide" }).optional(),
+    email: z
+      .string()
+      .email({ message: "L'email n'est pas valide" })
+      .max(255, { message: "L'email ne doit pas dépasser 255 caractères." })
+      .optional(),
+
     password: z
       .string()
       .min(8, {
         message: "Votre mot de passe doit contenir au moins 8 caractères.",
+      })
+      .max(255, {
+        message: "Le mot de passe ne doit pas dépasser 255 caractères.",
       })
       .refine(
         (val) =>
@@ -24,10 +32,14 @@ export const userUpdateSchema = z
       .string()
       .uuid({ message: "Le rôle doit être un UUID valide." })
       .optional(),
+
     username: z
       .string()
       .min(3, {
         message: "Le nom d'utilisateur doit contenir au moins 3 caractères.",
+      })
+      .max(50, {
+        message: "Le nom d'utilisateur ne doit pas dépasser 50 caractères.",
       })
       .optional(),
   })
